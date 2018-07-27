828f85132351a67f308bf92a719598912761b601
maximo_piu <maximo_piu18@hotmaill.com>
2018-07-26 20:45:12 -0500

    -Implementacion de servicio Rest y Api Key
    -Incorporacion listview
    -Incorporacion de Lib Volley Rest .jar
    -Incorporacion de Permisos

--- app/src/main/java/com/examle/listadorest/MainActivity.java
+++ app/src/main/java/com/examle/listadorest/MainActivity.java
@@ -1,18 +1,78 @@
 package com.examle.listadorest;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JsResult;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-    ListView lista;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends Activity {
+    String TAG="Main Activity";
+    RequestQueue requestQueue;
+     public static  ListView lista;
+    String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyDqLifooX1uMPIHleIFsugUkZPptfU1zzw";
+    String Lugares[];
+    public static ArrayAdapter<String> adapterJson;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lista = (ListView)findViewById(R.id.main_Listview);
-
+        ConsultaRest();
     }
+
+    public void ConsultaRest(){
+        requestQueue = Volley.newRequestQueue(this);
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jObject = new JSONObject(response);
+                    JSONArray jResults = jObject.getJSONArray("results");
+                    Log.e(TAG,"results: " + jResults.toString());
+
+                    Lugares = new String[jResults.length()];
+                    for (int j = 0; j < jResults.length(); j++) {
+                        Lugares[j] = "Lugar : " +j;
+                        JSONObject jPlaceObj = jResults.getJSONObject(j);
+                        JSONObject jGeometry =  jPlaceObj.getJSONObject("geometry");
+                        JSONObject jLocation = jGeometry.getJSONObject("location");
+
+                        String lat = jLocation.getString("lat");
+                        String lng = jLocation.getString("lng");
+                        //Log.e(TAG,"lat" + lat);
+                        //Log.e(TAG,"lng" + lng);
+                        Log.e(TAG,"Lugar: " +Lugares[j]);
+                    }
+                    adapterJson = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,Lugares);
+                    lista.setAdapter(adapterJson);
+
+                } catch (JSONException e) {
+                    Log.e("error","error" + e);
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG,"Error"+ error);
+            }
+        });
+        requestQueue.add(getRequest);
+    }
+
 }
